7fed8980dd6e348aa3628e749e896fa1303f9345
Du Min Chen <dm.chen0113@gmail.com>
2021-09-08 13:26:24 -0400

    Finish redux one lab

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { incrementCount, decrementCount, resetCount, incrementEvenCount, incrementByCount } from "../actions/counterActions";
 
 const Counter = () => {
   const dispatch = useDispatch();
   const { count } = useSelector((state) => state);
+  const [customIncrement, setCustomIncrement] = useState("");
 
-  const onDecrement = () => {};
-  const onIncrement = () => {};
+  const onDecrement = () => { dispatch(decrementCount()) };
+  const onIncrement = () => { dispatch(incrementCount()) };
+  const onReset = () => { dispatch(resetCount()) };
+  const onIncrementEven = () => { dispatch(incrementEvenCount()) };
+  const onIncrementBy = (e) => {
+    e.preventDefault();
+    dispatch(incrementByCount(customIncrement));
+    setCustomIncrement("");
+  };
+  const handleChange = (e) => {
+    setCustomIncrement(Number(e.target.value));
+  }
 
   return (
     <div>
@@ -14,6 +26,14 @@ const Counter = () => {
       <p>
         <button onClick={onIncrement}>+</button>
         <button onClick={onDecrement}>-</button>
+      </p>
+      <p>
+        <button onClick={onReset}>reset</button><br />
+        <button onClick={onIncrementEven}>+ if even</button><br />
+        <form onSubmit={onIncrementBy}>
+          <input type="number" value={customIncrement} onChange={handleChange} required />
+          <button>+ by</button>
+        </form>
       </p>
     </div>
   );
