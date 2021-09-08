c9857d47d6bd8454bb123562d1363e1517d35364
Teyanna Earle <teyonna.earle92@gmail.com>
2021-09-08 13:10:24 -0400

    Add lab answers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.css";
 import App from "./App";
+import { devToolsEnhancer } from "redux-devtools-extension";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers"
 
+const store = createStore(rootReducer, devToolsEnhancer())
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
