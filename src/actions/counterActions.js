0dbec9d93ba400bee7fe1de281aba810987b2f6d
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-09-08 15:39:29 -0400

    Cypress Test Passing

--- src/actions/counterActions.js
+++ src/actions/counterActions.js
@@ -1,9 +1,24 @@
-import { DECREMENT, INCREMENT,  } from "./actionTypes";
+import {
+  DECREMENT,
+  INCREMENT,
+  RESET,
+  INCREMENT_IF_EVEN,
+  INCREMENT_BY
+} from './actionTypes'
 
 export const decrementCount = () => {
-  return { type: DECREMENT };
-};
+  return { type: DECREMENT }
+}
 
 export const incrementCount = () => {
-  return { type: INCREMENT };
-};
+  return { type: INCREMENT }
+}
+export const resetCount = () => {
+  return { type: RESET }
+}
+export const incrementIfEvenCount = () => {
+  return { type: INCREMENT_IF_EVEN }
+}
+export const incrementByCount = number => {
+  return { type: INCREMENT_BY, number: Number(number) }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
-import count from "./count";
+import count from './count'
 
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux'
 
-export default combineReducers({ count });
+export default combineReducers({ count })
