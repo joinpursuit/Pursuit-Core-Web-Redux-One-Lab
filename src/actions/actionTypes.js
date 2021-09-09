c6a1c34d2e0c0ef1b0d8c055665aa09bbdb7addd
Alexander <alexanderl96@yahoo.com>
2021-09-09 12:19:01 -0400

    done

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,2 +1,5 @@
 export const DECREMENT = "DECREMENT";
 export const INCREMENT = "INCREMENT";
+export const INCREMENT_BY = "INCREMENT_BY";
+export const INCREMENT_IF_EVEN = "INCREMENT_IF_EVEN";
+export const RESET = "RESET";
