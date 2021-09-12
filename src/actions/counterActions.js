07ebeca81b581bb958faa992895e6d2a16baa067
Jordan Bobadilla <jordanboba08@gmail.com>
2021-09-12 13:20:09 -0400

    complete redux lab part 1

--- src/actions/counterActions.js
+++ src/actions/counterActions.js
@@ -1,4 +1,4 @@
-import { DECREMENT, INCREMENT,  } from "./actionTypes";
+import { DECREMENT, INCREMENT, INCREMENT_BY, INCREMENT_IF_EVEN, RESET } from "./actionTypes";
 
 export const decrementCount = () => {
   return { type: DECREMENT };
@@ -7,3 +7,15 @@ export const decrementCount = () => {
 export const incrementCount = () => {
   return { type: INCREMENT };
 };
+
+export const resetCount = () => {
+  return { type: RESET };
+};
+
+export const incrementIfEvenCount = () => {
+  return { type: INCREMENT_IF_EVEN };
+};
+
+export const incrementByAmountCount = (amount) => {
+  return {amount: Number(amount), type: INCREMENT_BY}
+}
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,20 +1,57 @@
 import React from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import {
+  decrementCount,
+  incrementByAmountCount,
+  incrementCount,
+  incrementIfEvenCount,
+  resetCount,
+} from "../actions/counterActions";
 
 const Counter = () => {
+  const [amount, setAmount] = useState(0);
   const dispatch = useDispatch();
-  const { count } = useSelector((state) => state);
+  const { reduceCount } = useSelector((state) => state);
 
-  const onDecrement = () => {};
-  const onIncrement = () => {};
+  const onDecrement = () => {
+    console.log("minus", decrementCount());
+    dispatch(decrementCount());
+  };
+  const onIncrement = () => {
+    console.log("plus");
+    dispatch(incrementCount());
+  };
+
+  const onReset = () => {
+    dispatch(resetCount());
+  };
+
+  const onIncrementIfEven = () => {
+    dispatch(incrementIfEvenCount());
+  };
+
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const onIncrementByAmount = () => {
+    dispatch(incrementByAmountCount(amount));
+  };
 
   return (
     <div>
-      <p>count: {count}</p>
+      <p>count: {reduceCount}</p>
       <p>
         <button onClick={onIncrement}>+</button>
         <button onClick={onDecrement}>-</button>
+        <button onClick={onIncrementIfEven}>+ if even</button>
       </p>
+      <div>
+        <input value={amount} onChange={handleAmount} />
+        <button onClick={onIncrementByAmount}>+ by</button>
+      </div>
+      <button onClick={onReset}>reset</button>
     </div>
   );
 };
