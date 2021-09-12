76ec66dfcc64812d095393a2fc3f58127a7786df
Marsha Hall <mphall77@gmail.com>
2021-09-12 15:19:29 -0400

    up to if even

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,41 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import {
+	incrementCount,
+	decrementCount,
+	resetCount,
+	evenCount,
+	incrementByCount,
+} from "../actions/counterActions";
 
 const Counter = () => {
-  const dispatch = useDispatch();
-  const { count } = useSelector((state) => state);
+	const dispatch = useDispatch();
+	const { count } = useSelector((state) => state);
 
-  const onDecrement = () => {};
-  const onIncrement = () => {};
+	const onDecrement = useSelector((state) => state.decrementCount);
+	const onIncrement = useSelector((state) => state.incrementCount);
+	const onInput = useSelector((state) => state.incrementByCount);
 
-  return (
-    <div>
-      <p>count: {count}</p>
-      <p>
-        <button onClick={onIncrement}>+</button>
-        <button onClick={onDecrement}>-</button>
-      </p>
-    </div>
-  );
+	const even = count % 2 === 0 ? onEven : count;
+
+	const onEven = useSelector((state) => state.evenCount);
+
+	return (
+		<div>
+			<p>count: {count ? onIncrement : onDecrement}</p>
+			<p>
+				<button onClick={() => dispatch(incrementCount())}>+</button>
+				<button onClick={() => dispatch(decrementCount())}>-</button>
+				<button onClick={() => dispatch(resetCount())}>reset</button>
+
+				<button onClick={() => dispatch(onEven())}>+ if even</button>
+				<input
+					onChange={({ target }) => dispatch(incrementByCount(target.value))}
+					value={count}
+				/>
+			</p>
+		</div>
+	);
 };
 
 export default Counter;
--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,3 +1,25 @@
-export default function reduceCount(state = 0, action) {
-  return state;
-}
+import {
+	DECREMENT,
+	INCREMENT,
+	INCREMENT_BY,
+	INCREMENT_IF_EVEN,
+	RESET,
+} from "../actions/actionTypes";
+
+const countReducer = (state = 0, action) => {
+	switch (action.type) {
+		case DECREMENT:
+			return (state -= 1);
+		case INCREMENT:
+			return (state += 1);
+		case RESET:
+			return (state = 0);
+		case INCREMENT_BY:
+			return (state = action.payload);
+		case INCREMENT_IF_EVEN:
+			return (state += 1);
+		default:
+			return state;
+	}
+};
+export default countReducer;
