440b58ee084a8261c018136e77bee0709c373d14
Shoaib Dar <SDAR30@gmail.com>
2021-09-12 14:39:48 -0400

    finished jest tests

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { incrementCount, decrementCount, reset, incrementBY, incrementIfEven } from "../actions/counterActions"
 import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
   const dispatch = useDispatch();
   const { count } = useSelector((state) => state);
 
-  const onDecrement = () => {};
-  const onIncrement = () => {};
+  const onDecrement = () => {
+    dispatch(decrementCount())
+  };
+  const onIncrement = () => {
+    dispatch(incrementCount())
+  };
+
+  const incrementByNumber = () => {
+    dispatch(incrementBY())
+  }
+
+  const incrementIfEvenNumber = () => {
+    dispatch(incrementIfEven())
+  }
+
+  const resetScore = () => {
+    dispatch(reset())
+  }
+
 
   return (
     <div>
@@ -14,7 +32,13 @@ const Counter = () => {
       <p>
         <button onClick={onIncrement}>+</button>
         <button onClick={onDecrement}>-</button>
+        <button onClick={resetScore}>reset</button>
+        <button onClick={incrementIfEvenNumber}>increment if even</button>
       </p>
+      <form>
+        <input type="number"></input>
+        <button onClick={incrementByNumber}>INCREMENT BY THIS NUMBER</button>
+      </form>
     </div>
   );
 };
--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,3 +1,18 @@
+import { INCREMENT, DECREMENT, RESET, INCREMENT_BY, INCREMENT_IF_EVEN } from "../actions/actionTypes"
+
 export default function reduceCount(state = 0, action) {
-  return state;
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    case RESET:
+      return 0;
+    case INCREMENT_BY:
+      return state + action.amount;
+    case INCREMENT_IF_EVEN:
+      return (state % 2) ? state : state + 1;
+    default:
+      return state;
+  }
 }
