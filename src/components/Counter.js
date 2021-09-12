02c7305b51f87b3a167010cdd536869bc4b5fdf4
chidalgo1221 <carlos.hidalgo2017@mesacharter.org>
2021-09-12 12:42:26 -0400

    Finished Lab W Poonam

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import { decrementCount, incrementCount, resetCount} from "../actions/counterActions"
 const Counter = () => {
   const dispatch = useDispatch();
   const { count } = useSelector((state) => state);
 
-  const onDecrement = () => {};
-  const onIncrement = () => {};
+  const onDecrement = () => {
+    dispatch(decrementCount())
+  };
+  const onIncrement = () => {
+    dispatch(incrementCount())
+  };
+
+  const onReset = () => {
+    dispatch(resetCount())
+  };
 
   return (
     <div>
@@ -14,6 +22,7 @@ const Counter = () => {
       <p>
         <button onClick={onIncrement}>+</button>
         <button onClick={onDecrement}>-</button>
+        <button onClick={onReset}>reset</button>
       </p>
     </div>
   );
--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,3 +1,14 @@
+import { DECREMENT, RESET } from "../actions/actionTypes";
+import { INCREMENT } from "../actions/actionTypes";
 export default function reduceCount(state = 0, action) {
+  switch (action.type){
+    case DECREMENT:
+      return state - 1
+    case INCREMENT:
+        return state + 1
+        case RESET:
+          return state = 0
+      default:  
   return state;
+  }
 }
